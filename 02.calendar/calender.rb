404cfb55ba7dfdbab841a13423f635fceda4e03b
git taro <git.taro@example.com>
2022-11-11 13:01:18 +0900

    fix01

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -1,22 +1,21 @@
 require 'date'
 require 'optparse'
-require 'debug' # 例: ここで読み込んでおく
-
+require 'debug'
 
 #今年の西暦を取得
 today = Date.today
 target_date = {year: today.year, month: today.month, day: today.day}
 
-#ユーザーからの入力を取得しtmp_dateに入力
-tmp_date    = ARGV.getopts('', 'year:', 'month:')
+#ユーザーからの入力を取得しoptionsに入力
+options    = ARGV.getopts('', 'year:', 'month:')
 
 #もし、ユーザーからの入力があった際にはtarget_dateを更新
-if tmp_date['year']
-  target_date[:year]   = tmp_date['year'].to_i
+if options['year']
+  target_date[:year]   = options['year'].to_i
 end
 
-if tmp_date['month'] 
-  target_date[:month]  = tmp_date['month'].to_i
+if options['month'] 
+  target_date[:month]  = options['month'].to_i
 end
 
 #月初と、月末日を取得
@@ -37,29 +36,15 @@ puts week.join(" ")
 # 初めの曜日まで空白を入れる。
 print "   " * firstday.wday
 
-# n = firstday.day + 1
-# n = n.to_s
-# print n.rjust(5, " ")
-
 
 (firstday..lastday).each do |date|
-  # binding.break # 例: ここに記入
-  if    date.sunday?
-    print date.day
-  elsif date.day == 1 #1日だけ、半角スペースを入れる。
-    print date.day.to_s.rjust(2)
-  elsif date.day <10  #10日未満は半角スペースを2つ入れる。
-    print date.day.to_s.rjust(3)
-  else                    #10日以上は半角スペースを1つ入れる。
-    print date.day.to_s.rjust(3)
-  end  
+  day = date.day.to_s.rjust(2)
 
   #土曜日の場合には改行を入れる。
   if date.saturday?
-    print "\n"
-    if date.day + 1 <10 #次の日付が、9以下なら、空白スペースを入れる
-      print " "
-    end
+    print day + "\n"
+  else
+    print day + " "
   end
 end
 
