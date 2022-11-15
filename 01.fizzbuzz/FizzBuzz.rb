57882c12b7fe1b3225acba1c0d907196cff40d4e
git taro <git.taro@example.com>
2022-11-15 11:14:10 +0900

    fix fizzbuzz01

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,6 +1,5 @@
-# 1から20までの数をプリントするプログラムを書け。ただし3の倍数のときは数の代わりに｢Fizz｣と、5の倍数のときは｢Buzz｣とプリントし、3と5両方の倍数の場合には｢FizzBuzz｣とプリントすること。
 1.upto(20) do |num|
-  if num % 3 ==0 && num % 5 == 0
+  if num % 3 == 0 && num % 5 == 0
     puts("FizzBuzz")
   elsif num % 3 == 0
     puts("Fizz")
