3d0565852fb108096a424651255d5920f1a7dea7
git taro <git.taro@example.com>
2022-11-17 17:03:52 +0900

    fix bowling00

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,51 +1,46 @@
 # frozen_string_literal: true
-
-THROW_NUM = ARGV[0].split(',')
+require 'debug'
+throw = ARGV[0].split(',')
 current_num = 0
 total_score = []
 
-def tenframe(total_score, framecount, current_num)
-  total_score[framecount] = if THROW_NUM[current_num] == 'X'
-                              if THROW_NUM[current_num + 1] == 'X' && THROW_NUM[current_num + 2] == 'X'
-                                30
-                              elsif THROW_NUM[current_num + 1] == 'X'
-                                20 + THROW_NUM[current_num + 2].to_i
-                              else
-                                10 + THROW_NUM[current_num + 1].to_i + THROW_NUM[current_num + 2].to_i
-                              end
-                            elsif THROW_NUM[current_num].to_i + THROW_NUM[current_num + 1].to_i == 10
-                              10 + THROW_NUM[current_num + 2].to_i
-                            else
-                              THROW_NUM[current_num].to_i + THROW_NUM[current_num + 1].to_i
-                            end
+def ten_frame(*ten_throw)
+  score = 0
+  i = 0
+  while ten_throw[i] do
+    if ten_throw[i] == 'X'
+      score += 10
+    else
+      score += ten_throw[i].to_i
+    end
+    i = i.next
+  end
+  score
 end
 
-10.times do |framecount|
-  if framecount < 9
-    if THROW_NUM[current_num] == 'X'
-      total_score[framecount] = if THROW_NUM[current_num + 1] == 'X' && THROW_NUM[current_num + 2] == 'X'
-                                  30
-                                elsif THROW_NUM[current_num + 1] == 'X'
-                                  20 + THROW_NUM[current_num + 2].to_i
-                                else
-                                  10 + THROW_NUM[current_num + 1].to_i + THROW_NUM[current_num + 2].to_i
-                                end
-      current_num += 1
-    else
-      total_score[framecount] = THROW_NUM[current_num].to_i + THROW_NUM[current_num + 1].to_i
-      current_num += 2
-    end
-
-    if total_score[framecount] == 10
-      total_score[framecount] += if THROW_NUM[current_num] == 'X'
-                                   10
-                                 else
-                                   THROW_NUM[current_num].to_i
-                                 end
-    end
+9.times do |frame_count|
+  if throw[current_num] == 'X'
+    total_score[frame_count] = if throw[current_num + 1] == 'X' && throw[current_num + 2] == 'X'
+                                30
+                              elsif throw[current_num + 1] == 'X'
+                                20 + throw[current_num + 2].to_i
+                              else
+                                10 + throw[current_num + 1].to_i + throw[current_num + 2].to_i
+                              end
+    current_num += 1
   else
-    tenframe(total_score, framecount, current_num)
+    total_score[frame_count] = throw[current_num].to_i + throw[current_num + 1].to_i
+    current_num += 2
+  end
+
+  if total_score[frame_count] == 10
+    total_score[frame_count] += if throw[current_num] == 'X'
+                                  10
+                                else
+                                  throw[current_num].to_i
+                                end
   end
 end
 
-puts total_score.inject(:+)
+total_score[9] = ten_frame(throw[current_num],throw[current_num + 1],throw[current_num + 2])
+puts total_score.sum
