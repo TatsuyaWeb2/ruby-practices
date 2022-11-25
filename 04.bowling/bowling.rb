f248626cf7506d19536200a7c7b78f14fa03ce72
git taro <git.taro@example.com>
2022-11-25 13:33:08 +0900

    fix bowling02

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,46 +1,44 @@
 # frozen_string_literal: true
 
-throw = ARGV[0].split(',')
-current_num = 0
+throw_score = ARGV[0].split(',')
 total_score = []
 
 def ten_frame(*ten_throw)
-  score = 0
-  i = 0
-  while ten_throw[i] do
-    if ten_throw[i] == 'X'
-      score += 10
-    else
-      score += ten_throw[i].to_i
-    end
-    i = i.next
-  end
+  score = ten_throw.sum {|x| 
+            if x == "X"
+              x = 10
+            else
+              x.to_i
+            end
+          }  
   score
 end
 
+throw_count = 0
+
 9.times do |frame_count|
-  if throw[current_num] == 'X'
-    total_score[frame_count] = if throw[current_num + 1] == 'X' && throw[current_num + 2] == 'X'
+  if throw_score[throw_count] == 'X'
+    total_score[frame_count] = if throw_score[throw_count + 1] == 'X' && throw_score[throw_count + 2] == 'X'
                                 30
-                              elsif throw[current_num + 1] == 'X'
-                                20 + throw[current_num + 2].to_i
+                              elsif throw_score[throw_count + 1] == 'X'
+                                20 + throw_score[throw_count + 2].to_i
                               else
-                                10 + throw[current_num + 1].to_i + throw[current_num + 2].to_i
+                                10 + throw_score[throw_count + 1].to_i + throw_score[throw_count + 2].to_i
                               end
-    current_num += 1
+    throw_count += 1
   else
-    total_score[frame_count] = throw[current_num].to_i + throw[current_num + 1].to_i
-    current_num += 2
+    total_score[frame_count] = throw_score[throw_count].to_i + throw_score[throw_count + 1].to_i
+    throw_count += 2
   end
 
   if total_score[frame_count] == 10
-    total_score[frame_count] += if throw[current_num] == 'X'
+    total_score[frame_count] += if throw_score[throw_count] == 'X'
                                   10
                                 else
-                                  throw[current_num].to_i
+                                  throw_score[throw_count].to_i
                                 end
   end
 end
 
-total_score[9] = ten_frame(throw[current_num],throw[current_num + 1],throw[current_num + 2])
+total_score[9] = ten_frame(throw_score[throw_count],throw_score[throw_count + 1],throw_score[throw_count + 2])
 puts total_score.sum
