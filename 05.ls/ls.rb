b5d6c5685c99663ad220a5ae2571a294d4689161
git taro <git.taro@example.com>
2022-11-30 15:33:24 +0900

    ls fix01

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,30 +1,31 @@
 # frozen_string_literal: true
 
-columns = 3
+COLUMN = 3
 
-file_name = Dir.glob('*').sort
+target_path = ARGV[0]
 
-# filenameの長さを見るメソッド
-def maximum_char(file_name)
-  max_char = 0
-  file_name.each do |name|
-    max_char = name.length if name.length > max_char
-  end
-  max_char
+if target_path == nil
+  file_names = Dir.glob('*').sort
+else
+  file_names = Dir.glob(target_path + '/*').sort
+  file_names = file_names.map{|x| x.sub(target_path + "/", '')}
 end
 
-# 順番に出力するメソッド
-def output(file_name, columns, max)
-  max_row = (file_name.size / columns.to_f).ceil
-  file_name = file_name.each_slice(max_row).to_a
+def maximum_char(file_names)
+  file_names.max.length
+end
+
+def output(file_names, columns, max)
+  max_row = (file_names.size / columns.to_f).ceil
+  file_names = file_names.each_slice(max_row).to_a
   (0..max_row - 1).each do |row|
-    (0..file_name.length - 1).each do |column| # (0..columns-1)にするとファイルが16個の場合、5列を表示できないため、file_name.lengthにしている。
-      print file_name[column][row].to_s.ljust(max) if file_name[column][row]
+    (0..file_names.length - 1).each do |columns| # (0..columns-1)にするとファイルが16個の場合、5列を表示できないため、file_name.lengthにしている。
+      print file_names[columns][row].to_s.ljust(max) if file_names[columns][row]
     end
     print "\n"
   end
 end
 
-max = maximum_char(file_name) + 2
+max = maximum_char(file_names) + 2
 
-output(file_name, columns, max)
+output(file_names, COLUMN, max)
