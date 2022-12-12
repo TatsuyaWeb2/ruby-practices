8356e4e4c078896d6f74d5cc1138a7ae5a387fc5
git taro <git.taro@example.com>
2022-12-12 13:58:59 +0900

    ls fix04

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-COLUMN = 4
+COLUMN = 3
 
-target_path = ARGV
+target_path = ARGV.sort
 
 def output(file_names, column, max)
   max_row = (file_names.size / column.to_f).ceil
@@ -10,33 +10,41 @@ def output(file_names, column, max)
   (0..max_row - 1).each do |row|
     (0..file_names.length - 1).each do |target_column| # (0..column-1)にするとファイルが16個の場合、5列を表示できないため、file_names.lengthにしている。
       print file_names[target_column][row].to_s.ljust(max) if file_names[target_column][row]
-      # binding.break
     end
     print "\n"
   end
+  print "\n"
+end
+
+def no_name(target_path)
+  truth_paths = []
+  target_path.each do |path|
+    if FileTest.exist?(path)
+      truth_paths.push(path)
+    else
+      puts("ls: #{path}: No such file or directory")
+    end
+  end
+  truth_paths
 end
 
 if target_path.empty?
   file_names = Dir.glob('*').sort
-  max = file_names.max.length + 2
+  max = file_names.max_by(&:length)
   output(file_names, COLUMN, max)
-
 else
-  target_path.map do |path|
+  truth_paths = no_name(target_path)
+end
+
+truth_paths.each do |path|
+  if target_path.length > 1
     if FileTest.directory?(path)
       puts "#{path}:"
     elsif FileTest.file?(path)
-      puts path
-    else
-      print("ls: #{path}: No such file or directory")
-    end
-
-    file_names = Dir.glob("#{path}/*").sort.map { |x| x.sub("#{path}/", '') }
-    if file_names.empty?
-      print ''
-    else
-      max = file_names.max.length + 2
-      output(file_names, COLUMN, max)
+      put path
     end
   end
+  file_names = Dir.glob("#{path}/*").sort.map { |x| x.sub("#{path}/", '') }
+  max = file_names.max_by(&:length).length + 3
+  output(file_names, COLUMN, max)
 end
